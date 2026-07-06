16425ba84871b0753c4f184134c733fcde447fd6
shailendr pawar <shailendrapawar980@gmail.com>
2026-07-06 14:14:55 +0530

    updated: claude context with new changes, restructred: frontend folder

--- frontend/src/components/layouts/RootLayout.tsx
+++ frontend/src/components/layouts/RootLayout.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Button } from '../ui/button'
 
 const RootLayout = () => {
   return (
-       <div
+    <div
       className={`relative h-screen min-h-screen w-full min-w-full bg-gray-700 text-white`}
-    >ewcwc
+    >
+
+      <Button variant="destructive">wjenwken</Button>
       {/* root layout */}
       <Outlet />
     </div>
